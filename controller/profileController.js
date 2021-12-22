b147897223e951219ce605d96da5be7fcefc1f37
S.Daniil <salyndaniil@gmail.com>
2021-12-22 05:31:33 +0300

    'changes'

--- controller/profileController.js
+++ controller/profileController.js
@@ -1,8 +1,30 @@
 const db = require('../db')
 const pool = require('../index')
-
 module.exports.employeeComplete = async (req,res) => {
-    await pool.connections[0].query(`SELECT employeeComplete(${req.body.iss},'${req.body.employee_name}', '{${req.body.skills}}')`).then(result =>{
+    console.log(`SELECT employeeComplete(${req.body.iss},
+        '${req.body.employee_name}',
+        '{${req.body.skills}},
+        '${req.body.about}',
+        '${req.body.gender}',
+        '${req.body.age}',
+        '${req.body.city}',
+        '${req.body.country}',
+        '${req.body.pos}',
+        '${req.body.yearsOfWork}',
+        '${req.body.educationName}',
+        '${req.body.educationLevel}')`)
+    await pool.connections[0].query(`SELECT employeeComplete(${req.body.iss},
+        '${req.body.employee_name}',
+        '{${req.body.skills}}',
+        '${req.body.about}',
+        '${req.body.gender}',
+        '${req.body.age}',
+        '${req.body.city}',
+        '${req.body.country}',
+        '${req.body.pos}',
+        '${req.body.yearsOfWork}',
+        '${req.body.educationName}',
+        '${req.body.educationLevel}')`).then(result =>{
         res.status(200).send('ok')
     }).catch(e => {
         console.error(e.stack)
@@ -13,6 +35,15 @@ module.exports.employeeComplete = async (req,res) => {
 module.exports.companyComplete = async (req,res) => {
     await pool.connections[1].query(`SELECT companyComplete(${req.body.iss},'${req.body.company_name}')`).then(result =>{
         res.status(200).send('ok')
+    }).catch(e => {
+        console.error(e.stack)
+        res.status(405).send('notok')
+    })
+}
+
+module.exports.companyGetEmployee = async(req,res) => {
+    await pool.connections[1].query(`SELECT * FROM getEmployeeData(${req.params.id}) as x(employee emptype)`).then(result => {
+        res.status(200).send(result.rows[0])
     }).catch(e => {
         console.error(e.stack)
         res.status(405).send('notok')
--- controller/vacancyController.js
+++ controller/vacancyController.js
@@ -20,13 +20,38 @@ module.exports.vacancies = async (req, res) => {
 }
 
 module.exports.vacancy = async (req,res) => {
-    console.log('Im here!!', req.params.id)
     vacancy = await pool.connections[0].query(`SELECT getVacancy(${req.params.id});`).then(res=>{
         return res.rows
     }).catch(e => {
         console.error(e.stack)
     })
     res.send(vacancy)
+}
+
+module.exports.apply = async (req,res) => {
+    await pool.connections[0].query(`SELECT apply(${req.body.iss},${req.body.vac})`).then(() => {
+        res.status(200).send('ok')
+    }).catch(e => {
+        console.error(e.stack)
+    })
+}
+
+module.exports.getapplied = async (req, res) => {
+    console.log('iss',req.body.iss)
+    await pool.connections[0].query(`SELECT getApplied(${req.params.id})`).then(result => {
+        res.status(200).send(result.rows[0].getapplied)
+    }).catch(e => {
+        console.error(e.stack)
+    })
+}
+
+module.exports.getvacancyresponses = async (req, res) => {
+    console.log(`SELECT companyGetApplied(${req.params.id})`)
+    await pool.connections[1].query(`SELECT companyGetApplied(${req.params.id})`).then(result => {
+        res.status(200).send(result.rows[0].companygetapplied)
+    }).catch(e => {
+        console.error(e.stack)
+    })
 }
 
 module.exports.createVacancy = async (req, res) => {
@@ -53,7 +78,6 @@ module.exports.createVacancy = async (req, res) => {
         res.status(200).send('ok')
         return res.rows
     }).catch(e => {
-        console.log('FKOBGOKIEDRBKOERKO')
         console.error(e.stack)
         res.status(403).send('notok')
     })
